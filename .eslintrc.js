c4465febb665bfeddf79f1fa7e479dbc75e63aed
Карина Фарадян <faradiankarina@gmail.com>
2022-11-29 04:43:38 +0400

    added ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    overrides: [],
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {},
+  };
